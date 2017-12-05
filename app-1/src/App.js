c1c99492df3591a9ba20fe0d9038e782a8f560c4
Aaron.Nacua <alnacua@gmail.com>
2017-12-04 19:56:16 -0600

    app 1 and app 2 Done

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,16 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    constructor(){
+      super()
+
+      this.state = {
+        userInput: ''
+
+      }
+   
+    }
+handleChange(value){
+  this.setState({userInput: value});
+
+}
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>REACT-DRILLS</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type='text'onChange={(event) => this.handleChange(event.target.value)}></input>
+        <h1>{this.state.userInput}</h1>
+        
       </div>
     );
   }
--- app-2/src/App.js
+++ app-2/src/App.js
@@ -2,7 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+
+
+constructor(){
+
+  super();
+
+  this.state={
+    list: ['pencil',
+     'pen', 
+     'paper',
+     'eraser',
+     'white-out'
+    ]
+
+
+
+  }
+}
+
+
+
+
   render() {
     return (
       <div className="App">
@@ -10,9 +34,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1><ul>{this.state.list.map((listValue) => 
+          <li>{listValue}</li>
+        )}</ul></h1>
       </div>
     );
   }
